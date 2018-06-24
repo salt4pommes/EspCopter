b572ec722a1d9205488cd299323f9d8a1c31f3a8
Willi Kreißl <hummel621@googlemail.com>
2018-06-24 14:19:40 +0200

    Small cleanups and code beautification for much better code readability.

--- src/esp.cpp
+++ src/esp.cpp
@@ -6,10 +6,8 @@
 #include <ArduinoOTA.h>
 
 extern "C" {
-  #include <espnow.h>
+#include <espnow.h>
 }
-
-#define FIXED_IP false
 
 #ifdef FIXED_IP
 uint8_t ip_address[4] = {192, 168, 178, 99};
@@ -33,12 +31,12 @@ volatile int peernum = 0;
 #define MIDRUD 1519
 #define THRCORR 19
 
-enum ang { ROLL,PITCH,YAW };
+enum ang { ROLL, PITCH, YAW };
 
 static int16_t gyroADC[3];
 static int16_t accADC[3];
 static int16_t gyroData[3];
-static float angle[2]    = {0,0};
+static float angle[2]    = {0, 0};
 extern int calibratingA;
 
 #define ROL 0
@@ -47,7 +45,7 @@ extern int calibratingA;
 #define RUD 3
 #define AU1 4
 #define AU2 5
-static int16_t rcCommand[] = {0,0,0};
+static int16_t rcCommand[] = {0, 0, 0};
 
 #define GYRO     0
 #define STABI    1
@@ -64,70 +62,66 @@ uint8_t armct = 0;
 #include "lib/mpu6050.cpp"
 #include "lib/telemetry.cpp"
 
-void recv_cb(u8 *macaddr, u8 *data, u8 len)
-{
-  recv = true;
-  //Serial.print("recv_cb ");
-  //Serial.println(len);
-  if (len == RCdataSize)
-  {
-    for (int i=0;i<RCdataSize;i++) RCdata.data[i] = data[i];
-  }
-  if (!esp_now_is_peer_exist(macaddr))
-  {
-    Serial.println("adding peer ");
-    esp_now_add_peer(macaddr, ESP_NOW_ROLE_COMBO, WIFI_CHANNEL, NULL, 0);
-    peernum++;
-  }
+void recv_cb(u8 *macaddr, u8 *data, u8 len) {
+    recv = true;
+    //Serial.print("recv_cb ");
+    //Serial.println(len);
+    if (len == RCdataSize) {
+        for (int i = 0; i < RCdataSize; i++) RCdata.data[i] = data[i];
+    }
+    if (!esp_now_is_peer_exist(macaddr)) {
+        Serial.println("adding peer ");
+        esp_now_add_peer(macaddr, ESP_NOW_ROLE_COMBO, WIFI_CHANNEL, NULL, 0);
+        peernum++;
+    }
 };
 
-void send_cb(uint8_t* mac, uint8_t sendStatus)
-{
-  //Serial.print("send_cb ");
+void send_cb(uint8_t *mac, uint8_t sendStatus) {
+    //Serial.print("send_cb ");
 };
 
 void setup() {
 
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  MPU6050_init();
-  MPU6050_readId(); // must be 0x68, 104dec
+    MPU6050_init();
+    MPU6050_readId(); // must be 0x68, 104dec
 
-  EEPROM.begin(64);
-  if (EEPROM.read(63) != 0x55) Serial.println("Need to do ACC calib");
-  ACC_Read();
+    EEPROM.begin(64);
+    if (EEPROM.read(63) != 0x55) Serial.println("Need to do ACC calib");
+    ACC_Read();
 
 //   WiFi.mode(WIFI_STA);
 //   WiFi.begin(ssid, password);
 
-  WiFi.mode(WIFI_STA); // Station mode for esp-now
-  WiFi.disconnect();
+    WiFi.mode(WIFI_STA); // Station mode for esp-now
+    WiFi.disconnect();
 
 //   while (WiFi.waitForConnectResult() != WL_CONNECTED) {}
 
-  if (esp_now_init() != 0) Serial.println("*** ESP_Now init failed");
+    if (esp_now_init() != 0) Serial.println("*** ESP_Now init failed");
 
-  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+    esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
 
-  esp_now_register_recv_cb(recv_cb);
-  esp_now_register_send_cb(send_cb);
+    esp_now_register_recv_cb(recv_cb);
+    esp_now_register_send_cb(send_cb);
 
 //   ArduinoOTA.onEnd([]() {
 //     Serial.println("\nEnd");
 //   });
 
-  #ifdef FIXED_IP
+#ifdef FIXED_IP
     IPAddress ip(ip_address);
     IPAddress gateway(ip_gateway);
     IPAddress subnet(ip_netmask);
     WiFi.config(ip, gateway, subnet);
-  #endif
+#endif
 
 //   ArduinoOTA.setHostname(chip_name);
 //   ArduinoOTA.begin();
 
-  delay(1000);
-  initServo();
+    delay(1000);
+    initServo();
 }
 
 uint32_t rxt; // receive time, used for falisave
@@ -135,121 +129,111 @@ uint32_t rxt; // receive time, used for falisave
 void loop() {
 //   ArduinoOTA.handle();
 
-  uint32_t now,diff;
+    uint32_t now, diff;
 
-  //now = millis(); // actual time
+    //now = millis(); // actual time
 
-  if (recv)
-  {
-    recv = false;
-    buf_to_rc();
+    if (recv) {
+        recv = false;
+        buf_to_rc();
 
-    if      (rcValue[AU1] < 1300) flightmode = GYRO;
-    else if (rcValue[AU1] > 1700) flightmode = RTH;
-    else                          flightmode = STABI;
-    if (oldflightmode != flightmode)
-    {
-      zeroGyroI();
-      oldflightmode = flightmode;
+        if (rcValue[AU1] < 1300) flightmode = GYRO;
+        else if (rcValue[AU1] > 1700) flightmode = RTH;
+        else                          flightmode = STABI;
+        if (oldflightmode != flightmode) {
+            zeroGyroI();
+            oldflightmode = flightmode;
+        }
+
+#if defined (ARMSWITCH)
+        if (armed) {
+            if (rcValue[AU2] <= 1400) {
+                armed = false;
+                armct = 0;
+            }
+            rcValue[THR]    -= THRCORR;
+            rcCommand[ROLL]  = rcValue[ROL] - MIDRUD;
+            rcCommand[PITCH] = rcValue[PIT] - MIDRUD;
+            rcCommand[YAW]   = rcValue[RUD] - MIDRUD;
+        } else if (rcValue[AU2] >= 1600) {
+            if (rcValue[THR] < MINTHROTTLE) armct++;
+            if (armct >= 25) armed = true;
+        }
+#else
+        if (armed) {
+            rcValue[THR]    -= THRCORR;
+            rcCommand[ROLL]  = rcValue[ROL] - MIDRUD;
+            rcCommand[PITCH] = rcValue[PIT] - MIDRUD;
+            rcCommand[YAW]   = rcValue[RUD] - MIDRUD;
+        } else {
+            if (rcValue[THR] < MINTHROTTLE) armct++;
+            if (armct >= 25) armed = true;
+        }
+#endif
+
+        //Serial.println(rcValue[AU2]    );
+        //Serial.print(rcValue[THR]    ); Serial.print("  ");
+        //Serial.print(rcCommand[ROLL] ); Serial.print("  ");
+        //Serial.print(rcCommand[PITCH]); Serial.print("  ");
+        //Serial.print(rcCommand[YAW]  ); Serial.println();
+
+        //diff = now - rxt;
+        //Serial.print(diff); Serial.println();
+        rxt = millis();
+
+        if (peernum > 0) {
+            //t_angle();
+            t_gyro();
+            //t_acc();
+
+            //esp_now_send(NULL, (u8*)hello, sizeof(hello)); // NULL means send to all peers
+        }
     }
 
-    #if defined (ARMSWITCH)
-    if (armed)
-    {
-      if (rcValue[AU2] <= 1400) { armed = false; armct = 0; }
-      rcValue[THR]    -= THRCORR;
-      rcCommand[ROLL]  = rcValue[ROL] - MIDRUD;
-      rcCommand[PITCH] = rcValue[PIT] - MIDRUD;
-      rcCommand[YAW]   = rcValue[RUD] - MIDRUD;
-    }
-    else if (rcValue[AU2] >= 1600)
-    {
-      if (rcValue[THR] < MINTHROTTLE) armct++;
-      if (armct >= 25) armed = true;
-    }
-    #else
-    if (armed)
-    {
-      rcValue[THR]    -= THRCORR;
-      rcCommand[ROLL]  = rcValue[ROL] - MIDRUD;
-      rcCommand[PITCH] = rcValue[PIT] - MIDRUD;
-      rcCommand[YAW]   = rcValue[RUD] - MIDRUD;
-    }
-    else
-    {
-      if (rcValue[THR] < MINTHROTTLE) armct++;
-      if (armct >= 25) armed = true;
-    }
-    #endif
+    Gyro_getADC();
+    //Serial.print(gyroADC[0]); Serial.print("  ");
+    //Serial.print(gyroADC[1]); Serial.print("  ");
+    //Serial.print(gyroADC[2]); Serial.println("  ");
 
-    //Serial.println(rcValue[AU2]    );
-    //Serial.print(rcValue[THR]    ); Serial.print("  ");
-    //Serial.print(rcCommand[ROLL] ); Serial.print("  ");
-    //Serial.print(rcCommand[PITCH]); Serial.print("  ");
-    //Serial.print(rcCommand[YAW]  ); Serial.println();
+    ACC_getADC();
+    //Serial.print(accADC[0]); Serial.print("  ");
+    //Serial.print(accADC[1]); Serial.print("  ");
+    //Serial.print(accADC[2]); Serial.println("  ");
 
-    //diff = now - rxt;
+    getEstimatedAttitude();
+    //Serial.print(angle[0]); Serial.print("  ");
+    //Serial.print(angle[1]); Serial.println("  ");
+
+    pid();
+
+    mix();
+
+    writeServo();
+
+    // Failsave part
+    if (now > rxt + 90) {
+        rcValue[THR] = MINTHROTTLE;
+        rxt = now;
+        //Serial.println("FS");
+    }
+
+    // parser part
+    if (Serial.available()) {
+        char ch = Serial.read();
+        // Perform ACC calibration
+        if (ch == 'A') {
+            Serial.println("Doing ACC calib");
+            calibratingA = 64; // CALSTEPS
+            while (calibratingA != 0) {
+                delay(CYCLETIME);
+                ACC_getADC();
+            }
+            ACC_Store();
+            Serial.println("ACC calib Done");
+        }
+    }
+
+    delay(CYCLETIME - 1);
+    //diff = millis() - now;
     //Serial.print(diff); Serial.println();
-    rxt = millis();
-
-    if (peernum > 0)
-    {
-      //t_angle();
-      t_gyro();
-      //t_acc();
-
-      //esp_now_send(NULL, (u8*)hello, sizeof(hello)); // NULL means send to all peers
-    }
-  }
-
-  Gyro_getADC();
-  //Serial.print(gyroADC[0]); Serial.print("  ");
-  //Serial.print(gyroADC[1]); Serial.print("  ");
-  //Serial.print(gyroADC[2]); Serial.println("  ");
-
-  ACC_getADC();
-  //Serial.print(accADC[0]); Serial.print("  ");
-  //Serial.print(accADC[1]); Serial.print("  ");
-  //Serial.print(accADC[2]); Serial.println("  ");
-
-  getEstimatedAttitude();
-  //Serial.print(angle[0]); Serial.print("  ");
-  //Serial.print(angle[1]); Serial.println("  ");
-
-  pid();
-
-  mix();
-
-  writeServo();
-
-  // Failsave part
-  if (now > rxt+90)
-  {
-    rcValue[THR] = MINTHROTTLE;
-    rxt = now;
-    //Serial.println("FS");
-  }
-
-  // parser part
-  if (Serial.available())
-  {
-    char ch = Serial.read();
-    // Perform ACC calibration
-    if (ch == 'A')
-    {
-      Serial.println("Doing ACC calib");
-      calibratingA = 64; // CALSTEPS
-      while (calibratingA != 0)
-      {
-        delay(CYCLETIME);
-        ACC_getADC();
-      }
-      ACC_Store();
-      Serial.println("ACC calib Done");
-    }
-  }
-
-  delay(CYCLETIME-1);
-  //diff = millis() - now;
-  //Serial.print(diff); Serial.println();
 }
